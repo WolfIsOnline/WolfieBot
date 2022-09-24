7f142317fe31a5c0d7b7268f998ba01642533350
wolfisonline <cmzpoison@gmail.com>
2022-09-24 04:06:51 -0400

    added join and leave logs

--- core/modlogs.py
+++ core/modlogs.py
@@ -1,10 +1,13 @@
+import discord
+import pytz
+
 from doctest import debug_script
 from email import message
-import discord
 from discord.ext import commands
 from discord.commands import slash_command
-from pyparsing import empty
 from database.guild_db import Guild_DataBase
+
+
 
 class ModLogs(commands.Cog):
     def __init__(self, bot):
@@ -16,7 +19,7 @@ class ModLogs(commands.Cog):
         guild_id = ctx.guild.id
         channel_id = ctx.channel.id
         db.update_guild_key(guild_id, "modlog_channel", channel_id)
-        await ctx.respond("Channel set")
+        await ctx.respond("mod logs will now display in this channel")
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
@@ -30,13 +33,37 @@ class ModLogs(commands.Cog):
             return
         await self.send_msglog(before, "Message was edited", 0x738adb, after.content)
 
+    @slash_command(description="test")
+    async def test_embed(self, ctx):
+        member = ctx.author
+        embed = discord.Embed(title="User Left", description=f"{member} has left the server", color=0xffffff)
+        embed.set_author(name=f"{member}", icon_url=member.display_avatar)
+        embed.set_footer(text=f"Account ID: {member.id}")
+        await self.bot.get_channel(ctx.channel.id).send(embed=embed)
+
     @commands.Cog.listener()
-    async def on_member_join(member):
-        pass
+    async def on_member_remove(self, member):
+        embed = discord.Embed(title="User Left", description=f"{member} has left the server", color=0xffffff)
+        embed.set_author(name=f"{member}", icon_url=member.display_avatar)
+        embed.set_footer(text=f"Account ID: {member.id}")
+        db = Guild_DataBase()
+        channel = db.get_guild_key(member.guild.id, "modlog_channel")
+        await self.bot.get_channel(int(channel)).send(embed=embed)
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        embed = discord.Embed(title="User Joined", description=f"{member.mention} joined the server", color=0x034f06)
+        embed.set_author(name=f"{member}", icon_url=member.display_avatar)
+        embed.add_field(name=f"Account created", value=member.created_at.astimezone(pytz.timezone("US/Eastern")).strftime("%c"))
+        embed.set_footer(text=f"Account ID: {member.id}")
+        db = Guild_DataBase()
+        channel = db.get_guild_key(member.guild.id, "modlog_channel")
+        await self.bot.get_channel(int(channel)).send(embed=embed)
+
 
     async def send_msglog(self, message, action, color, after = None):
         log_embed = discord.Embed(title=action, description=message.content, color=color)
-        log_embed.set_author(name=message.author, icon_url=message.author.avatar.url)
+        log_embed.set_author(name=message.author, icon_url=message.author.display_avatar)
         if after != None:
             log_embed.add_field(name="After edit", value=str(after), inline=False)
         log_embed.add_field(name=f"Message author:", value=message.author, inline=True)

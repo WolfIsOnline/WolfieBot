761d5e6c2f40322f29e659ed247dc770a32003c0
WolfIsOnline <cmzpoison@gmail.com>
2022-09-29 08:23:18 +0000

    patch support

---
+++ core/patches.py
@@ -0,0 +1,73 @@
+import discord
+
+from discord import slash_command
+from discord.ext import commands
+from rich import print
+from rich import pretty
+
+class Patches(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(description="Testing")
+    async def embed_getter(self, ctx):
+        message = await fetch_message(1024895333295214612)
+        print(message)
+        await ctx.respond("succcesss")
+
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.channel.id == 1024949304546316299:
+            return
+        if message.author.id == 1021246946649849867:
+            return
+        
+        original = message.embeds
+        data = None
+        for m in original:
+            data = m.to_dict()
+
+        try: 
+            if data["title"] == "PatchBot can now notify you of free games" or data["title"] == "Pinned Updates with PatchBot Premium":
+                return
+            title = data["title"] 
+        except: title = ""
+
+        try: description = data["description"]
+        except: description = ""
+
+        try: author = data["author"]["name"]
+        except: author = ""
+
+        try: thumbnail = data["thumbnail"]["url"]
+        except: thumbnail = ""
+        
+        try: image = data["image"]["url"]
+        except: image = ""
+
+        try:
+            fields = [] 
+            fields = data["fields"]
+        except: fields = []
+        
+        duplicate = discord.Embed(title=title, description=description, color=0x02e7e7)
+        duplicate.set_author(name=author)
+        duplicate.set_thumbnail(url=thumbnail)
+        duplicate.set_image(url=image)
+
+        for c in fields:
+            duplicate.add_field(name=c["name"], value=c["value"], inline=c["inline"])
+        
+        if author == "Epic Games" or author == "Steam" or author == "GOG":
+            channel = self.bot.get_channel(967074827108245554)
+        else:
+            channel = self.bot.get_channel(973745210016280577)
+        await channel.send(embed=duplicate)
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Patches(bot))

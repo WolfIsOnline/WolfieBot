31edca1acc8e5714caae72f4f85b8e67b41e837d
WolfIsOnline <cmzpoison@gmail.com>
2022-09-30 05:24:55 +0000

    made welcome display even if the guild has no icon

--- core/welcome.py
+++ core/welcome.py
@@ -21,10 +21,16 @@ class Welcome(commands.Cog):
     async def on_member_join(self, member):
         guild = member.guild.name
         member_name = member.id
+        try: guild_icon = member.guild.icon.url
+        except: 
+            wolfie = await self.bot.fetch_user(1021246946649849867)
+            guild_icon = wolfie.display_avatar
         welcome = discord.Embed(title=f"Welcome to {guild}", description=f"Hi <@{member_name}> \n\n• **I hope you enjoy your stay!**\n\n• **Check out the following channels.**\n\n <a:ng_bluearrowright:996149975471902760> Get your roles from <#983869250768883722>\n\n<a:ng_bluearrowright:996149975471902760> Current free games <#967074827108245554>\n\n<a:ng_bluearrowright:996149975471902760> Newest <#973745210016280577>", color=self.utils.DEFAULT_COLOR)
-        welcome.set_author(name=guild, icon_url=member.guild.icon.url)
+        welcome.set_author(name=guild, icon_url=guild_icon)
         welcome.set_thumbnail(url=member.display_avatar)
-        welcome.set_image(url=member.guild.banner.url)
+        try: welcome.set_image(url=member.guild.banner.url) 
+        except: pass
+
         channel = self.bot.get_channel(int(db.get_guild_key(member.guild.id, "welcome_channel")))
         await channel.send(embed=welcome)
 

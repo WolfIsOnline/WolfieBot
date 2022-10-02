17b6d90a6351159e35905337bbeba4f924dca510
WolfIsOnline <65690199+WolfIsOnline@users.noreply.github.com>
2022-10-02 03:15:26 -0400

    Update database.py

--- database/database.py
+++ database/database.py
@@ -13,49 +13,9 @@ from rich import print
 from rich import pretty
 
 
+
 log = logging.getLogger("rich")
-
-class Guild_DataBase:
-    def __init__(self):
-        load_dotenv(find_dotenv())
-        MONGODB_CONNECTION = os.environ.get("MONGODB_CONNECTION")
-        self.client = MongoClient(MONGODB_CONNECTION)
-
-    def add_guild(self, user_id):
-        try:
-            self.client.guilds.create_collection(str(user_id))
-            log.info(f"{user_id} has been added")
-        except CollectionInvalid:
-            log.info(f"{user_id} already exists")
-    
-    def update_guild_key(self, user_id, id, value):
-        document = self.client["guilds"][str(user_id)]
-        document.find_one_and_update({"_id": str(id)},{"$set": {"value": str(value)}},upsert=True)
-
-    def add_guild_key(self, user_id, id, value):
-        try:
-            document = self.client["guilds"][str(user_id)]
-            data = {"_id": str(id), "value": str(value)}
-            document.insert_one(data)
-            log.info(f"{id} has been added")
-
-        except DuplicateKeyError:
-            log.info(f"{id} is a duplicate")
-
-    def delete_guild_key(self, user_id, id, value):
-        document = self.client["guilds"][str(user_id)]
-        document.find_one_and_delete({"_id": id},{"value": value})
-        log.info(f"{id} has been deleted")
-
-    def get_guild_key(self, user_id, id):
-        document = self.client["guilds"][str(user_id)]
-        results = document.find({"_id":str(id)})
-        value = "None"
-        for x in results:
-            value = x["value"]
-        return value
-
-class User_Database:
+class _User_Database:
     def __init__(self):
         load_dotenv(find_dotenv())
         MONGODB_CONNECTION = os.environ.get("MONGODB_CONNECTION")
@@ -132,8 +92,50 @@ class User_Database:
             value = x["value"]
         return value
 
+class Guild_DataBase:
+    def __init__(self):
+        load_dotenv(find_dotenv())
+        MONGODB_CONNECTION = os.environ.get("MONGODB_CONNECTION")
+        self.client = MongoClient(MONGODB_CONNECTION)
+
+    def add_guild(self, user_id):
+        try:
+            self.client.guilds.create_collection(str(user_id))
+            log.info(f"{user_id} has been added")
+        except CollectionInvalid:
+            log.info(f"{user_id} already exists")
+    
+    def update_guild_key(self, user_id, id, value):
+        document = self.client["guilds"][str(user_id)]
+        document.find_one_and_update({"_id": str(id)},{"$set": {"value": str(value)}},upsert=True)
+
+    def add_guild_key(self, user_id, id, value):
+        try:
+            document = self.client["guilds"][str(user_id)]
+            data = {"_id": str(id), "value": str(value)}
+            document.insert_one(data)
+            log.info(f"{id} has been added")
+
+        except DuplicateKeyError:
+            log.info(f"{id} is a duplicate")
+
+    def delete_guild_key(self, user_id, id, value):
+        document = self.client["guilds"][str(user_id)]
+        document.find_one_and_delete({"_id": id},{"value": value})
+        log.info(f"{id} has been deleted")
+
+    def get_guild_key(self, user_id, id):
+        document = self.client["guilds"][str(user_id)]
+        results = document.find({"_id":str(id)})
+        value = "None"
+        for x in results:
+            value = x["value"]
+        return value
+
+
+
 def test_prgram():
-    db = User_Database()
+    db = _User_Database()
     while True:
         print("1) Add Transaction")
         print("2) Add x100 Transactions")

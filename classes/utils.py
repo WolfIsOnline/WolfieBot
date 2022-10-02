ee635cff7ac44efa9525e4043fd570bdc1310d5a
WolfIsOnline <65690199+WolfIsOnline@users.noreply.github.com>
2022-10-02 03:11:47 -0400

    Update utils.py

--- classes/utils.py
+++ classes/utils.py
@@ -13,9 +13,18 @@ class Utils:
             return True
         return False
 
-    async def notify(self, ctx, notify_msg):
-        embed = discord.Embed(title=None, description=notify_msg, color=self.DEFAULT_COLOR)
+    async def notify(self, ctx, prompt, info, author, thumbnail="https://images-ext-1.discordapp.net/external/pq6uWSdWFXqOyDcktN0qSBzSkL3Txrk1gEGyoLmeXXE/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1021246946649849867/118da25b10cf765d4472c4664df8dd63.png?width=468&height=468"):
+        embed = discord.Embed(title=prompt, description=info, color=self.DEFAULT_COLOR)
+        embed.set_thumbnail(url=thumbnail)
+        embed.set_author(name=author)
         await ctx.respond(embed=embed)
+
+    async def notify_channel(self, channel, title, message, name):
+        embed = discord.Embed(title=title, description=message, color=self.DEFAULT_COLOR)
+        embed.set_thumbnail(url=channel.guild.icon.url)
+        embed.set_author(name=name)
+        await channel.send(embed=embed)
+
 
     async def is_owner(self, ctx):
         if ctx.author.id == 257646822925926410:

10fa9dfef51883ef392b6faa69599fec1cb3fb80
wolfisonline <cmzpoison@gmail.com>
2022-10-05 18:38:47 -0400

    updated notify_channel()

--- classes/utils.py
+++ classes/utils.py
@@ -3,6 +3,8 @@ import discord
 from database.database import GuildDatabase
 
 gd = GuildDatabase()
+
+THUMBNAIL = "https://images-ext-1.discordapp.net/external/pq6uWSdWFXqOyDcktN0qSBzSkL3Txrk1gEGyoLmeXXE/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1021246946649849867/118da25b10cf765d4472c4664df8dd63.png?width=468&height=468"
 class Utils:
     
     DEFAULT_COLOR = 0x02e7e7
@@ -13,16 +15,16 @@ class Utils:
             return True
         return False
 
-    async def notify(self, ctx, prompt, info, author, thumbnail="https://images-ext-1.discordapp.net/external/pq6uWSdWFXqOyDcktN0qSBzSkL3Txrk1gEGyoLmeXXE/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1021246946649849867/118da25b10cf765d4472c4664df8dd63.png?width=468&height=468"):
+    async def notify(self, ctx, prompt, info, author, thumbnail=THUMBNAIL):
         embed = discord.Embed(title=prompt, description=info, color=self.DEFAULT_COLOR)
         embed.set_thumbnail(url=thumbnail)
         embed.set_author(name=author)
         await ctx.respond(embed=embed)
 
-    async def notify_channel(self, channel, title, message, name):
-        embed = discord.Embed(title=title, description=message, color=self.DEFAULT_COLOR)
-        embed.set_thumbnail(url=channel.guild.icon.url)
-        embed.set_author(name=name)
+    async def notify_channel(self, channel, prompt, info, author, thumbnail=THUMBNAIL):
+        embed = discord.Embed(title=prompt, description=info, color=self.DEFAULT_COLOR)
+        embed.set_thumbnail(url=thumbnail)
+        embed.set_author(name=author)
         await channel.send(embed=embed)
 
 

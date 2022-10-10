60a7126a6b0f3b44e06e7ea3c8e1a577012a53eb
wolfisonline <cmzpoison@gmail.com>
2022-10-10 13:35:59 -0400

    easy bot setup instance

---
+++ botsetup.py
@@ -0,0 +1,15 @@
+async def set_token():
+    
+    token = input("Enter bot token: ")
+    with open(".env", "w") as env:
+        env.write(f"TOKEN={token}")
+    print(f"{env.name} written")    
+    await set_database()
+    
+async def set_database():
+    database = input("Enter MongoDB connection: ")
+    with open("./database/.env", "w") as env:
+        env.write(f"MONGODB_CONNECTION=\"{database}\"")
+    print(f"{env.name} written")
+    
+set_token()

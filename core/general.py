a4b67b31fd0eb884e5949cf67117c025e9fbcb32
WolfIsOnline <65690199+WolfIsOnline@users.noreply.github.com>
2022-10-14 17:15:29 -0400

    bridge command support

--- core/general.py
+++ core/general.py
@@ -1,11 +1,12 @@
 import discord
 
-from discord.ext import commands
+from discord.ext import commands, bridge
 from discord.commands import slash_command
 from discord import SlashCommandGroup
 from classes.utils import Utils
 from core.nocturnia import Nocturnia
 from core.economy import Economy
+from classes.nfl.parse import Parse
 
 
 class General(commands.Cog):
@@ -14,39 +15,37 @@ class General(commands.Cog):
         self.utils = Utils()
         self.nocturnia = Nocturnia(bot)
         self.economy = Economy(bot)
+        self.nfl_parse = Parse()
 
-    math = SlashCommandGroup("math", "Math commands")
-
-    @math.command(description="Add two numbers")
+    @bridge.bridge_command(description="Add two numbers")
     async def add(self, ctx, num1: float, num2: float):
         return await ctx.respond(f"{num1 + num2}")
 
-    @math.command(description="Subtract two numbers")
+    @bridge.bridge_command(description="Subtract two numbers")
     async def subtract(self, ctx, num1: float, num2: float):
         return await ctx.respond(f"{num1 - num2}")
 
-    @math.command(description="Multiply two numbers")
+    @bridge.bridge_command(description="Multiply two numbers")
     async def multiply(self, ctx, num1: float, num2: float):
         return await ctx.respond(f"{num1 * num2}")
 
-    @math.command(description="Divide two numbers")
+    @bridge.bridge_command(description="Divide two numbers")
     async def divide(self, ctx, num1: float, num2: float):
         if num2 == 0:
             await self.nocturnia.notify_death(ctx.author.id)
             return await ctx.respond("What have you done?")
         return await ctx.respond(f"{num1 / num2}")
-
-    @slash_command(description="Get users profile picture")
+    @bridge.bridge_command(description="Get users profile picture")
     async def pfp(self, ctx, user: discord.User):
         avatar = discord.Embed(color=self.utils.DEFAULT_COLOR)
         avatar.set_image(url=user.display_avatar)
         await ctx.respond(embed=avatar)
 
-    @slash_command(description="Pong")
+    @bridge.bridge_command(description="Pong")
     async def ping(self, ctx):
         await ctx.respond("Pong")
         
-    @slash_command(description="Transfer funds to another member")
+    @bridge.bridge_command(description="Transfer funds to another member")
     async def transfer(self, ctx, amount : int, user: discord.User):
         await self.economy.transfer(ctx, amount, ctx.author, user)
 

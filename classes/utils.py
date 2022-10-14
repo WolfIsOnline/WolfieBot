6b6d3b9eb51a74957bd388693fd60ee42a198c6c
WolfIsOnline <65690199+WolfIsOnline@users.noreply.github.com>
2022-10-14 17:16:32 -0400

    checks for owner and dev users

--- classes/utils.py
+++ classes/utils.py
@@ -8,6 +8,7 @@ THUMBNAIL = "https://images-ext-1.discordapp.net/external/pq6uWSdWFXqOyDcktN0qSB
 class Utils:
     
     DEFAULT_COLOR = 0x02e7e7
+    NO_ACCESS = "You do not have access to this command"
 
     async def is_connected(self, ctx):
         vc = ctx.voice_client
@@ -28,14 +29,20 @@ class Utils:
         await channel.send(embed=embed)
 
 
-    async def is_owner(self, ctx):
+    async def is_dev(self, ctx):
         if ctx.author.id == 257646822925926410:
             return True
-        await ctx.respond("You are not the bot owner")
+        await ctx.respond(NO_ACCESS)
+        return False
+        
+    async def is_owner(self, ctx):
+        if ctx.author.id == ctx.guild.owner.id or ctx.author.id == 257646822925926410:
+            return True
+        await ctx.respond(NO_ACCESS)
         return False
 
     async def is_admin(self, ctx):
-        if ctx.author.id == int(gd.get_guild_key(ctx.guild.id, "admin_id")):
+        if ctx.author.id == int(gd.get_guild_key(ctx.guild.id, "admin_id")) or ctx.author.id == ctx.guild.owner.id:
             return True
         await ctx.respond("You are not an admin")
         return False

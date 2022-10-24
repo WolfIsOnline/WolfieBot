8b6eb2a1c489b9275c394837465f0a52ed4a1c57
wolfisonline <cmzpoison@gmail.com>
2022-10-24 16:44:13 -0400

    version bump

--- cogs/info.py
+++ cogs/info.py
@@ -5,8 +5,8 @@ from discord.ext import commands
 from discord.commands import slash_command
 from classes.utils import Utils
 
-
 class Info(commands.Cog):
+    BOT_VERSION = "1.1.3-alpha"
     def __init__(self, bot):
         self.bot = bot
         self.utils = Utils()
@@ -16,13 +16,12 @@ class Info(commands.Cog):
         python_version = sys.version
         pycord = "development version"
         creator = "WolfIsOnline#6677"
-        bot_version = "1.1.1-alpha"
         info = discord.Embed(color=self.utils.DEFAULT_COLOR)
         info.set_author(name="Wolfie#1704", icon_url="https://cdn.discordapp.com/avatars/1021246946649849867/118da25b10cf765d4472c4664df8dd63.png?size=1024")
         info.add_field(name="Bot created by", value=creator)
         info.add_field(name="Python", value=python_version)
         info.add_field(name="Pycord", value=pycord)
-        info.add_field(name="Wolfie version", value=bot_version)
+        info.add_field(name="Wolfie version", value=self.BOT_VERSION)
 
         await ctx.respond(embed=info)
 

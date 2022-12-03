c328c5bcaccd09ec01f264a2977df5802337c591
WolfIsOnline <65690199+WolfIsOnline@users.noreply.github.com>
2022-12-03 13:06:47 -0500

    removed

--- main.py
+++ main.py
@@ -59,41 +59,6 @@ async def on_ready():
     print(f"Bot is currently in {guilds} servers")
     print(f"{bot.user} has connected to discord")
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="becoming sentient"))
-    await update_notify()
-
-async def update_notify():
-    gd = GuildDatabase()
-    utils = Utils()
-    info = Info(bot)
-    version = info.BOT_VERSION
-    guild_count = 0
-    for c in bot.guilds:
-        guild_count += 1
-        if gd.get_guild_key(c.id, "update_notify") == "None":
-            gd.update_guild_key(c.id, "update_notify", False)
-        if gd.get_guild_key(c.id, "update_notify") == "False":
-            gd.update_guild_key(c.id, "update_notify", True)
-            embed = discord.Embed(title="New Update", description=f"Updated to {version}", color=utils.DEFAULT_COLOR)
-            channel_id = db.get_guild_key(c.id, "modlog_channel")
-            channel = c.get_channel(int(channel_id))
-            await channel.send(embed=embed)
-    
-async def changelog():
-    utils = Utils()
-    with open("changelog.txt", "r") as change:
-        changeall = change.read()
-        
-    guild_count = 0
-    for c in bot.guilds:
-        guild_count += 1
-    for c, guild in enumerate(bot.guilds):
-        print(guild.id)
-        channel_id = db.get_guild_key(guild.id, "modlog_channel")
-        channel = guild.get_channel(int(channel_id))
-        embed = discord.Embed(title="NEW UPDATE 1.0.0-alpha", description=changeall, color=utils.DEFAULT_COLOR)
-        await channel.send(embed=embed)
-        print(f"changelog sent to {guild} {c + 1}/{guild_count}")
-
 
 load_cogs()
 load_dotenv(find_dotenv())

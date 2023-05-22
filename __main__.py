389935f53219fd49d03343c1df2130483eea3604
wolfisonline <cmzpoison@gmail.com>
2023-05-22 05:02:26 -0400

    load .env in new format

--- __main__.py
+++ __main__.py
@@ -2,23 +2,26 @@ import hikari
 import lightbulb
 import os
 import logging
+import uvloop
 
 from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
 from hikari.events.base_events import EventT
 from lightbulb.errors import ExtensionNotFound
 from wolfiebot.database.database import Database
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
+DISCORD_API_KEY = os.environ.get("DISCORD_API_KEY")
+
 log = logging.getLogger(__name__)
-bot = lightbulb.BotApp(token=os.environ.get("TOKEN"), prefix="!", intents=hikari.Intents.ALL, default_enabled_guilds=1021249050445611009)
+bot = lightbulb.BotApp(token=DISCORD_API_KEY, prefix="!", intents=hikari.Intents.ALL, default_enabled_guilds=1021249050445611009)
 db = Database()
 
 @bot.listen(hikari.StartedEvent)
 async def start(event):
-    await bot.update_presence(status=hikari.Status.ONLINE, activity=hikari.Activity( name=db.read_user_data(bot.get_me().id, "status"), type=hikari.ActivityType.PLAYING,),)
+    await bot.update_presence(status=hikari.Status.ONLINE, activity=hikari.Activity(name=db.read_user_data(bot.get_me().id, "status"), type=hikari.ActivityType.PLAYING,),)
 
-core = ["quotes", "rooms", "logs", "welcome", "autorole", "chat"]
+core = ["quotes", "rooms", "logs", "welcome", "autorole", "chat", "voice"]
 for c in core:
     bot.load_extensions(f"wolfiebot.core.{c}")
 
@@ -38,6 +41,5 @@ async def on_command_error(event: lightbulb.CommandErrorEvent) -> None:
         await event.context.respond("You do not have access to this command")
     
 if __name__ == "__main__":
-    import uvloop
     uvloop.install()
     bot.run()

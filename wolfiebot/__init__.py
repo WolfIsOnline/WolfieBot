49d624356d548f0a3f63eda91fe322527ab10933
wolfisonline <cmzpoison@gmail.com>
2023-05-22 05:01:47 -0400

    get .env's

--- wolfiebot/__init__.py
+++ wolfiebot/__init__.py
@@ -1,6 +1,16 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
 __version__ = "2.0.0"
 __authors__ = "WolfIsOnline#6677"
 __description__ = "A multi-tool bot with an emphasis on economy"
+
+
+# load .env file
+load_dotenv(find_dotenv())
+MONGODB_CONNECTION = os.environ.get("MONGODB_CONNECTION")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+ELEVENLABS_API_KEY = os.environ.get("ELEVENLABS_API_KEY")
 
 DEFAULT_COLOR = 0x02e7e7
 CURRENCY_NAME = "Eurodollar"

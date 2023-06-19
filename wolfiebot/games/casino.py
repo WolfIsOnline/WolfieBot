4918577bf8195a66bd8782c2fb3e9c686cda86a4
wolfisonline <cmzpoison@gmail.com>
2023-06-19 19:48:55 -0400

    payout to bank

--- wolfiebot/games/casino.py
+++ wolfiebot/games/casino.py
@@ -25,11 +25,12 @@ async def balance(ctx: lightbulb.Context):
     balance = _casino.get_balance(author_id)
     description = f"Credits: **{wolfiebot.CASINO_SYMBOL}{balance:,}**"
     embed = hikari.Embed(title="Nocturnia Casino", description=description, color=wolfiebot.CASINO_COLOR)
+    embed.set_author(name=f"{ctx.author} | Member", icon=ctx.author.display_avatar_url)
     await ctx.respond(embed)
     
 
 @casino.child
-@lightbulb.option("amount", "Amount of money", type=int, required=True)
+@lightbulb.option("amount", "Amount to deposit", type=int, required=True)
 @lightbulb.command("deposit", "Deposit money into casino")
 @lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
 async def deposit(ctx: lightbulb.Context): 
@@ -46,6 +47,24 @@ async def deposit(ctx: lightbulb.Context):
     embed.add_field("Credits:", f"**{wolfiebot.CASINO_SYMBOL}{balance:,}**")
     await ctx.respond(embed)
     
+@casino.child
+@lightbulb.option("amount", "Amount to payout", type=int, required=True)
+@lightbulb.command("payout", "Payout casino money to your bank account")
+@lightbulb.implements(lightbulb.PrefixSubCommand, lightbulb.SlashSubCommand)
+async def payout(ctx: lightbulb.Context): 
+    author_id = ctx.author.id
+    balance = _casino.get_balance(author_id)
+    if balance < 1:
+        await ctx.respond("There is no money to payout")
+        return
+    
+    if ctx.options.amount > balance:
+        await ctx.respond(f"Amount requested is greater than your available payout")
+        return
+    
+    confirmed_amount = _casino.withdraw(author_id, ctx.options.amount)
+    bank.deposit(author_id, confirmed_amount, "Nocturnia Casino payout")
+    await ctx.respond(f"**{wolfiebot.CURRENCY_SYMBOL}{ctx.options.amount:,}** has been transfered to your bank")
     
 def load(bot: lightbulb.BotApp):
     bot.add_plugin(plugin)

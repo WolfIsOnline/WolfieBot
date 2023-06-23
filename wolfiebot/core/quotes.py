92db7f69ffaf71dc059dba0da7d2eeed1399794f
Cameron Kauffman <65690199+WolfIsOnline@users.noreply.github.com>
2023-06-23 09:59:30 -0400

    support for discord's new naming system

--- wolfiebot/core/quotes.py
+++ wolfiebot/core/quotes.py
@@ -2,65 +2,68 @@ import hikari
 import lightbulb
 import logging
 import wolfiebot
-import re
 
+from hikari.permissions import Permissions
 from wolfiebot.database.database import Database
 
 log = logging.getLogger(__name__)
-plugin = lightbulb.Plugin("core.quotes")
+plugin = lightbulb.Plugin("core.rooms")
 db = Database()
 
-def validate(quote) -> bool:
-    if quote:
-        quote = quote[0]
-        if quote == "" or quote.isspace():
-            return False
-        return True
-    return False
-
-def is_unknown(quote_user_id) -> bool:
-    if quote_user_id:
-        return False
-    return True
-
-async def commit(content, author_id, guild_id, ctx=None):
-    quote = re.split("\"|“|”", content)[1::2]
-    quote_user_id = re.split("<@|>", content)[1::2]
-    
-    if validate(quote) is True:
-        quote = quote[0]
-        submitted_user = await plugin.bot.rest.fetch_user(author_id)
-        if is_unknown(quote_user_id) is False:
-            quote_user_id = quote_user_id[0]
-            quote_user = await plugin.bot.rest.fetch_user(quote_user_id)
-            desc_format = quote_user.mention
-        else:
-            quote_user = "Unknown"
-            quote_user_id = -1
-            desc_format = "Unknown"
-    else:
-        return
-    
-    db.append_guild_data(guild_id, "quotes", {"quote" : quote, "quote_user_id" : int(quote_user_id), "quote_user" : str(quote_user), "submitted_user" : str(submitted_user), "submitted_user_id" : author_id})
-    total_quotes = len(db.read_guild_data(guild_id, "quotes"))
-    embed = hikari.Embed(title="Quote Added", description=f"\"{quote}\" - {desc_format}", color=wolfiebot.DEFAULT_COLOR)
-    embed.set_author(name=f"Quote #{total_quotes}")
-    if ctx is None:
-        await plugin.bot.rest.create_message(db.read_guild_data(guild_id, "quotes_channel"), embed)
-    else:
-        await ctx.respond(embed)
-
-@plugin.listener(hikari.GuildMessageCreateEvent)
+@plugin.listener(hikari.VoiceStateUpdateEvent)
 async def listen(event):
-    channel_id = db.read_guild_data(event.guild_id, "quotes_channel")
-    if event.channel_id != channel_id or event.is_bot is True:
+    parent_channel_id = db.read_guild_data(event.guild_id, "autoroom_parent")
+    if event.state.channel_id != parent_channel_id:
         return
-    if not event.content.startswith("\"") and not event.content.startswith("“"):
+    
+    await create_room(event, parent_channel_id)
+    
+@plugin.listener(hikari.VoiceStateUpdateEvent)
+async def check_empty(event):
+    if event.old_state is None:
         return
-    await commit(event.content, event.author_id, event.guild_id)
+    channels = db.read_guild_data(event.guild_id, "autoroom_channels")
+    for _id in channels:
+        if event.old_state.channel_id == _id:
+            channel = plugin.bot.cache.get_guild_channel(_id)
+            members = plugin.bot.cache.get_voice_states_view_for_channel(event.guild_id, channel)
+            if len(members) == 0:
+                await channel.delete()
+                db.remove_guild_data_array(event.guild_id, "autoroom_channels", _id)
+    
+async def create_room(event, parent_channel_id):
+    parent_channel = plugin.bot.cache.get_guild_channel(parent_channel_id)
+    member = plugin.bot.cache.get_member(event.guild_id, event.state.user_id)
+    if member.nickname is None:
+        room_name = member
+    else:
+        room_name = member.nickname
+    
+    channel = await event.app.rest.create_guild_voice_channel(
+            event.guild_id, 
+            f"{room_name}'s Room", 
+            user_limit=parent_channel.user_limit,
+            bitrate=parent_channel.bitrate,
+            video_quality_mode=parent_channel.video_quality_mode,
+            region=parent_channel.region,
+            category=parent_channel.parent_id,
+            reason=f"Requested by {plugin.bot.get_me()}",
+            permission_overwrites=[hikari.channels.PermissionOverwrite(
+                id=event.state.user_id, 
+                type=hikari.channels.PermissionOverwriteType.MEMBER,
+                allow=(Permissions.MANAGE_CHANNELS),
+            )]
+    )
+    db.append_guild_data(event.guild_id, "autoroom_channels", channel.id)
+    await plugin.bot.rest.edit_member(event.guild_id, event.state.user_id, voice_channel=channel.id)
+    
+async def delete_empty(channel_id):
+    pass
 
 def load(bot: lightbulb.BotApp):
     bot.add_plugin(plugin)
 
 def unload(bot: lightbulb.BotApp):
     bot.remove_plugin(plugin)
+    
+    

d78c4ca58517d19652116d1432d4320b3d8921da
Cameron Kauffman <65690199+WolfIsOnline@users.noreply.github.com>
2023-06-23 09:58:54 -0400

    added dm feature

--- wolfiebot/core/chat.py
+++ wolfiebot/core/chat.py
@@ -13,7 +13,6 @@ log = logging.getLogger(__name__)
 db = Database()
 openai.api_key = wolfiebot.OPENAI_API_KEY
 
-
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def check(event):
     if plugin.bot.get_me().id in event.message.user_mentions:
@@ -33,7 +32,7 @@ async def check(event):
         messages.append(prompt_data)
         
         await plugin.bot.rest.trigger_typing(channel)
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613", messages=messages)
         
         reply = response["choices"][0]["message"]["content"]
         await plugin.bot.rest.create_message(channel, reply)
@@ -41,7 +40,39 @@ async def check(event):
         
         if len(previous_messages) > 29:
             db.remove_user_data_array_by_index(user_id, "conversation", 0)
+            
+@plugin.listener(hikari.DMMessageCreateEvent)
+async def dm_check(event):
+    if event.author_id == plugin.bot.get_me().id:
+        return
+    
+    system_prompt = db.read_user_data(plugin.bot.get_me().id, "prompt")
+    user_id = event.author.id
+    channel = event.message.channel_id
+    content = event.message.content
+    parsed = re.sub(r"<.*?>", "", content)
+    prompt_data = {"role": "user", "content": parsed}
+    system_data = {"role": "system", "content": system_prompt}
+    db.append_user_data(user_id, "conversation", prompt_data)
+    messages = []
+    messages.append(system_data)
+    previous_messages = db.read_user_data(user_id, "conversation")
+    for c, line in enumerate(previous_messages):
+        messages.append(line)
+    messages.append(prompt_data)
         
+    await plugin.bot.rest.trigger_typing(channel)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613", messages=messages, temperature=1.2)
+        
+    reply = response["choices"][0]["message"]["content"]
+    await plugin.bot.rest.create_message(channel, reply)
+    db.append_user_data(user_id, "conversation", {"role": "assistant", "content": reply})
+        
+    if len(previous_messages) > 29:
+        db.remove_user_data_array_by_index(user_id, "conversation", 0)
+        
+    
+    
 def load(bot: lightbulb.BotApp):
     bot.add_plugin(plugin)
 

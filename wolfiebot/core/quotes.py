06e9708b7c78e942d79bb8d0d08475ec594c630d
Cameron Kauffman <65690199+WolfIsOnline@users.noreply.github.com>
2023-06-23 09:59:52 -0400

    find_one_and_update

--- wolfiebot/core/quotes.py
+++ wolfiebot/core/quotes.py
@@ -1,69 +1,72 @@
-import hikari
-import lightbulb
 import logging
+import os
 import wolfiebot
 
-from hikari.permissions import Permissions
-from wolfiebot.database.database import Database
+from pymongo import ASCENDING, MongoClient
 
 log = logging.getLogger(__name__)
-plugin = lightbulb.Plugin("core.rooms")
-db = Database()
 
-@plugin.listener(hikari.VoiceStateUpdateEvent)
-async def listen(event):
-    parent_channel_id = db.read_guild_data(event.guild_id, "autoroom_parent")
-    if event.state.channel_id != parent_channel_id:
-        return
+class Database:
     
-    await create_room(event, parent_channel_id)
-    
-@plugin.listener(hikari.VoiceStateUpdateEvent)
-async def check_empty(event):
-    if event.old_state is None:
-        return
-    channels = db.read_guild_data(event.guild_id, "autoroom_channels")
-    for _id in channels:
-        if event.old_state.channel_id == _id:
-            channel = plugin.bot.cache.get_guild_channel(_id)
-            members = plugin.bot.cache.get_voice_states_view_for_channel(event.guild_id, channel)
-            if len(members) == 0:
-                await channel.delete()
-                db.remove_guild_data_array(event.guild_id, "autoroom_channels", _id)
-    
-async def create_room(event, parent_channel_id):
-    parent_channel = plugin.bot.cache.get_guild_channel(parent_channel_id)
-    member = plugin.bot.cache.get_member(event.guild_id, event.state.user_id)
-    if member.nickname is None:
-        room_name = member
-    else:
-        room_name = member.nickname
-    
-    channel = await event.app.rest.create_guild_voice_channel(
-            event.guild_id, 
-            f"{room_name}'s Room", 
-            user_limit=parent_channel.user_limit,
-            bitrate=parent_channel.bitrate,
-            video_quality_mode=parent_channel.video_quality_mode,
-            region=parent_channel.region,
-            category=parent_channel.parent_id,
-            reason=f"Requested by {plugin.bot.get_me()}",
-            permission_overwrites=[hikari.channels.PermissionOverwrite(
-                id=event.state.user_id, 
-                type=hikari.channels.PermissionOverwriteType.MEMBER,
-                allow=(Permissions.MANAGE_CHANNELS),
-            )]
-    )
-    db.append_guild_data(event.guild_id, "autoroom_channels", channel.id)
-    await plugin.bot.rest.edit_member(event.guild_id, event.state.user_id, voice_channel=channel.id)
-    
-async def delete_empty(channel_id):
-    pass
+    def __init__(self):
+        self.client = MongoClient(wolfiebot.MONGODB_CONNECTION)
+        
+    def edit_user_data(self, user_id, name, value):
+        document = self.client["wolfie"]["users"]
+        document.find_one_and_update({"_id": user_id}, {"$set" : { name : value }}, upsert=True)
+        
+    def append_user_data(self, user_id, name, value):
+        document = self.client["wolfie"]["users"]
+        document.find_one_and_update({"_id" : user_id}, {"$push" : { name : value }}, upsert=True)
 
-def load(bot: lightbulb.BotApp):
-    bot.add_plugin(plugin)
-
-def unload(bot: lightbulb.BotApp):
-    bot.remove_plugin(plugin)
+    # this is to long but is very specific
+    # never use this unless you have no choice
+    def remove_user_data_array_by_index(self, user_id, name, index):
+        document = self.client["wolfie"]["users"]
+        
+        # this is a work around for removing an array by the index
+        # this can cause some future issues but for now it works
+        document.find_one_and_update({"_id" : user_id}, {"$unset": {f"{name}.{index}": 1}}, upsert=True)
+        document.find_one_and_update({"_id" : user_id}, {"$pull" : {f"{name}" : None}}, upsert=True)
+        
+    def read_user_data(self, user_id, name=None):
+        document = self.client["wolfie"]["users"]
+        raw_data = document.find({"_id" : user_id}, {})
+        for data in raw_data:
+            if name is not None:
+                is_present = name in data
+                if is_present is False:
+                    return None
+                return data[name]
+        return None
+        
+    def delete_user_data(self, user_id, name):
+        document = self.client["wolfie"]["users"]
+        document.find_one_and_update({"_id" : user_id}, { "$unset" : { name : {} } })
+        
+    def edit_guild_data(self, guild_id, name, value):
+        document = self.client["wolfie"]["guilds"]
+        document.find_one_and_update({"_id": guild_id}, {"$set" : { name : value }}, upsert=True)
+        
+    def append_guild_data(self, guild_id, name, value):
+        document = self.client["wolfie"]["guilds"]
+        document.find_one_and_update({"_id" : guild_id}, {"$push" : { name : value }}, upsert=True)   
+         
+    def remove_guild_data_array(self, guild_id, name, value):
+        document = self.client["wolfie"]["guilds"]
+        document.find_one_and_update({"_id" : guild_id}, {"$pull" : { name : value }}, upsert=True)   
+        
+    def read_guild_data(self, guild_id, name=None):
+        document = self.client["wolfie"]["guilds"]
+        raw_data = document.find({"_id" : guild_id}, {})
+        for data in raw_data:
+            if name is not None:
+                is_present = name in data
+                if is_present is False:
+                    return None
+                return data[name]
+        return raw_data
     
-    
+    def delete_guild_data(self, guild_id, name):
+        document = self.client["wolfie"]["guilds"]
+        document.update_one({"_id" : guild_id}, { "$unset" : { name : {} } })

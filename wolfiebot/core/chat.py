162992fbf19933a2dedd501128f787666c3288f2
Cameron Kauffman <65690199+WolfIsOnline@users.noreply.github.com>
2023-06-24 15:28:39 -0400

    rewrite of chat

--- wolfiebot/core/chat.py
+++ wolfiebot/core/chat.py
@@ -1,77 +1,99 @@
-import os
-import openai
 import hikari
 import lightbulb
 import logging
 import wolfiebot
+import requests
+import json
 import re
+import time
 
 from wolfiebot.database.database import Database
 
 plugin = lightbulb.Plugin("core.chat")
 log = logging.getLogger(__name__)
-db = Database()
-openai.api_key = wolfiebot.OPENAI_API_KEY
+database = Database()
+
+base_url = "http://localhost:3000"
 
 @plugin.listener(hikari.GuildMessageCreateEvent)
-async def check(event):
+async def guild_listen(event):
     if plugin.bot.get_me().id in event.message.user_mentions:
-        system_prompt = db.read_user_data(plugin.bot.get_me().id, "prompt")
-        user_id = event.author.id
-        channel = event.message.channel_id
-        content = event.message.content
-        parsed = re.sub(r"<.*?>", "", content)
-        prompt_data = {"role": "user", "content": parsed}
-        system_data = {"role": "system", "content": system_prompt}
-        db.append_user_data(user_id, "conversation", prompt_data)
-        messages = []
-        messages.append(system_data)
-        previous_messages = db.read_user_data(user_id, "conversation")
-        for c, line in enumerate(previous_messages):
-            messages.append(line)
-        messages.append(prompt_data)
+        user = plugin.bot.cache.get_member(event.guild_id, event.author.id)
+        name = user.nickname
+        if name is None:
+            name = user
+        await chat(event=event, user_name=name, user_id=user.id)
         
-        await plugin.bot.rest.trigger_typing(channel)
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613", messages=messages)
-        
-        reply = response["choices"][0]["message"]["content"]
-        await plugin.bot.rest.create_message(channel, reply)
-        db.append_user_data(user_id, "conversation", {"role": "assistant", "content": reply})
-        
-        if len(previous_messages) > 29:
-            db.remove_user_data_array_by_index(user_id, "conversation", 0)
-            
 @plugin.listener(hikari.DMMessageCreateEvent)
-async def dm_check(event):
+async def dm_listen(event):
     if event.author_id == plugin.bot.get_me().id:
         return
+    await chat(event=event, user_name=event.author, user_id=event.author.id)
     
-    system_prompt = db.read_user_data(plugin.bot.get_me().id, "prompt")
-    user_id = event.author.id
-    channel = event.message.channel_id
-    content = event.message.content
-    parsed = re.sub(r"<.*?>", "", content)
-    prompt_data = {"role": "user", "content": parsed}
-    system_data = {"role": "system", "content": system_prompt}
-    db.append_user_data(user_id, "conversation", prompt_data)
+async def chat(event, user_name: str, user_id: int):
+        channel = event.message.channel_id
+        session_id = establish_connection(user_id=user_id, user_name=str(user_name))
+        message = re.sub(r"<.*?>", "", event.message.content)
+        send_message(session_id=session_id, message=message)
+        await plugin.bot.rest.trigger_typing(channel)
+        time.sleep(3)
+        response = get_response(session_id=session_id)
+        close_connection(session_id=session_id)
+        await plugin.bot.rest.create_message(channel, response)
+    
+def send_message(session_id: str, message: str):
+    url = f"{base_url}/session/{session_id}/message"
+    message = {"message" : message}
+    response = requests.post(url, headers={"Content-Type" : "application/json"}, data=json.dumps(message))
+    
+    if response.status_code == 202:
+        log.info(f"message [{message}] sent successfully")
+    else:
+        log.error(f"message failed to send: {response.text}")
+    
+def close_connection(session_id: str):
+    url = f"{base_url}/http:/session/{session_id}/close"
+    response = requests.get(url, headers={}, data={})
+    log.info("connection closed")
+    
+def create_session(user_id: int, user_name: str) -> str:
+    url = "http://localhost:3000/session/open"
+    post_data = {
+        "uid" : str(user_id),
+        "sceneId" : "workspaces/default-firdbgosgclm_v_vu5dcnw/scenes/discord",
+        "characterId": "-1",
+        "playerName": user_name,
+    }
+    
+    response = requests.post(url, headers={"Content-Type" : "application/json"}, data=json.dumps(post_data))
+    
+    if response.status_code == 200:
+        data = response.json()
+        session_id = data["sessionId"]
+        
+        return session_id
+    
+def get_response(session_id: str):
+    url = f"{base_url}/events"
+    response = requests.get(url, headers={}, data={})
+    data = response.json()
     messages = []
-    messages.append(system_data)
-    previous_messages = db.read_user_data(user_id, "conversation")
-    for c, line in enumerate(previous_messages):
-        messages.append(line)
-    messages.append(prompt_data)
-        
-    await plugin.bot.rest.trigger_typing(channel)
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-0613", messages=messages, temperature=1.2)
-        
-    reply = response["choices"][0]["message"]["content"]
-    await plugin.bot.rest.create_message(channel, reply)
-    db.append_user_data(user_id, "conversation", {"role": "assistant", "content": reply})
-        
-    if len(previous_messages) > 29:
-        db.remove_user_data_array_by_index(user_id, "conversation", 0)
-        
+    for c in data:
+        if c["sessionId"] == session_id:
+            messages.append(c["text"])
+    message = "".join(messages)
+    log.info(f"reply {message}")
+    log.info(f"{data}")
+    return message
     
+def establish_connection(user_id: int, user_name: str) -> str:
+    session_id = create_session(user_id=user_id, user_name=user_name)
+    if session_id is None:
+        session_id = database.read_user_data(user_id=user_id, name="session_id")
+    else:
+        log.info(f"connection established: {session_id}")
+        database.edit_user_data(user_id=user_id, name="session_id", value=session_id)
+    return session_id
     
 def load(bot: lightbulb.BotApp):
     bot.add_plugin(plugin)

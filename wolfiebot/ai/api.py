c4a320b990ac2e514f84deb84c47d96413a37cdd
wolfisonline <cmzpoison@gmail.com>
2023-06-26 20:06:00 -0400

    resend message if fail

--- wolfiebot/ai/api.py
+++ wolfiebot/ai/api.py
@@ -43,8 +43,8 @@ class Api:
     def send_message(self, session_id: str, message: str, attempts: int = 3, _attempt: int = 1) -> str:
         response = requests.post(url=f"{host}/session/{session_id}/message", headers={"Content-Type" : "application/json"}, data=json.dumps({"message": message}))
         if response.status_code == 202:
-            time.sleep(3)
-            reply = self._get_response(session_id=session_id)
+            time.sleep(4)
+            reply = self._get_response(session_id=session_id, _message=message)
             return reply
         
         if response.status_code == 200:
@@ -52,9 +52,8 @@ class Api:
                 log.warning(f"message failed, retrying ({_attempt}/{attempts})")
                 time.sleep(3)
                 return self.send_message(session_id=session_id, message=message, attempts=attempts, _attempt=_attempt + 1)
-        return response.text
     
-    def _get_response(self, session_id: str, attempts: int = 10, _attempt: int = 1) -> str:
+    def _get_response(self, session_id: str, _message: str = "", attempts: int = 10, _attempt: int = 1) -> str:
         response = requests.get(url=f"{host}/events", headers={}, data={})
         data = response.json()
         messages = []
@@ -63,12 +62,17 @@ class Api:
             if c["sessionId"] == session_id:
                 if c["type"] == "text":
                     messages.append(c["text"])
+                elif c["type"] == "disconnected":
+                    if _attempt <= attempts:
+                        log.warning(f"message disconnected, resending ({_attempt}/{attempts}")
+                        time.sleep(2)
+                        return self.send_message(session_id=session_id, message=_message)
         message = "".join(messages)
         
         if message == "":
             if _attempt <= attempts:
-                log.warning(f"message is empty, retrying ({attempt}/{attempts})")
-                time.sleep(1)
+                log.warning(f"message is empty, retrying ({_attempt}/{attempts})")
+                time.sleep(2)
                 return self._get_response(session_id=session_id, attempts=attempts, _attempt=_attempt + 1)
         log.info(f"reply generated [{session_id}]")
         return message

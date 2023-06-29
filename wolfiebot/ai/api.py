02e80598c33dbe403a53df8fc0416e7b8dcce3cc
wolf <cmzpoison@gmail.com>
2023-06-29 08:46:20 -0400

    api

--- wolfiebot/ai/api.py
+++ wolfiebot/ai/api.py
@@ -4,26 +4,35 @@ import time
 import logging
 
 from typing import List
+from wolfiebot.database.database import Database
 
 host = "http://localhost:3000"
 log = logging.getLogger(__name__)
 
+SCENE_ID = "workspaces/default-firdbgosgclm_v_vu5dcnw/scenes/discord"
+CHARACTER_ID = "-1" 
+
+
+
 class Api:
     def __init__(self):
-        pass
+        self.database = Database()
     
     def get_server_status(self) -> str:
         response = requests.get(url=f"{host}/status", headers={}, data={})
         return response.text
     
     def open_session(self, user_id: str, scene_id: str, character_id: str, player_name: str, server_id: str) -> List[str]:
-        response = requests.post(url=f"{host}/session/open", headers={"Content-Type" : "application/json"}, data=json.dumps({"uid" : user_id, "sceneId" : scene_id, "playerName" : player_name, "serverId" : server_id}))
+        response = requests.post(url=f"{host}/session/open", headers={"Content-Type" : "application/json"}, data=json.dumps({"uid" : str(user_id), "sceneId" : scene_id, "playerName" : player_name, "serverId" : server_id}))
         if response.status_code == 200:
             data = response.json()
             session = {"id" : data["sessionId"], "character" : data["character"], "characters" : data["characters"]}
-        
+            session_id = data["sessionId"]
+            self.update_session_id(user_id=user_id, session_id=session_id)
+            log.info(f"opening session [{session_id}]")
         else:
             session = {"id" : response.status_code, "character" : response.status_code, "characters" : response.status_code}
+            log.error(f"{response.text} {response.status_code}")
         return session
     
     def close_session(self, session_id: str) -> bool:
@@ -38,24 +47,21 @@ class Api:
             return False
         return True
     
-    def send_message(self, session_id: str, message: str, attempts: int = 5, _attempt: int = 1) -> str:
+    def send_message(self, session_id: str, message: str, user_id: str, scene_id: str, character_id: str, player_name: str, server_id: str) -> str:
         response = requests.post(url=f"{host}/session/{session_id}/message", headers={"Content-Type" : "application/json"}, data=json.dumps({"message": message}))
         if response.status_code == 202:
-            time.sleep(4)
+            time.sleep(3)
             reply = self._get_response(session_id=session_id, _message=message)
             if reply == "disconnected":
-                if _attempt <= attempts:
-                    log.warning(f"message failed, retrying ({_attempt}/{attempts})")
-                    time.sleep(3)
-                    return self.send_message(session_id=session_id, message=message, attempts=attempts, _attempt=_attempt + 1)               
+                log.warning(f"message failed, retrying {reply}")
+                status = self.get_session_status(session_id=session_id)
+                if status is False:
+                    session = self.open_session(user_id=user_id, scene_id=scene_id, character_id=character_id, player_name=player_name, server_id=server_id)
+                    session_id = session["id"]
+                    return self.send_message(session_id=session_id, message=message, user_id=str(user_id), scene_id=SCENE_ID, character_id=CHARACTER_ID, player_name=player_name, server_id="")
             return reply
         
-        if response.status_code == 200:
-            if _attempt <= attempts:
-                log.warning(f"message failed, retrying ({_attempt}/{attempts})")
-                time.sleep(3)
-                return self.send_message(session_id=session_id, message=message, attempts=attempts, _attempt=_attempt + 1)
-    
+        
     def _get_response(self, session_id: str, _message: str = "", attempts: int = 10, _attempt: int = 1) -> str:
         response = requests.get(url=f"{host}/events", headers={}, data={})
         data = response.json()
@@ -77,3 +83,8 @@ class Api:
                 return self._get_response(session_id=session_id, attempts=attempts, _attempt=_attempt + 1)
         log.info(f"reply generated [{session_id}]")
         return message
+    
+    def update_session_id(self, user_id: int, session_id: str):
+        if session_id is None or session_id == 409:
+            return
+        self.database.edit_user_data(user_id=int(user_id), name="session_id", value=session_id)

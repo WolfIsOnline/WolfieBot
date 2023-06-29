908080dc31a0d1b54bfe15ffaa7f1d51fc074fad
wolf <cmzpoison@gmail.com>
2023-06-29 08:47:03 -0400

    formatting

--- wolfiebot/ai/chat.py
+++ wolfiebot/ai/chat.py
@@ -7,6 +7,8 @@ import json
 import re
 import time
 import asyncio
+import inspect
+import pprint
 
 from wolfiebot.database.database import Database
 from wolfiebot.ai.api import Api
@@ -38,25 +40,34 @@ async def chat(event, user_name: str, user_id: int, is_dm: bool) -> None:
     session_id = database.read_user_data(user_id=user_id, name="session_id")
     session_status = api.get_session_status(session_id)
     if session_status is False:
-        session = api.open_session(user_id=str(user_id), scene_id=SCENE_ID, character_id=CHARACTER_ID, player_name=str(user_name), server_id="")
+        session = api.open_session(user_id=user_id, scene_id=SCENE_ID, character_id=CHARACTER_ID, player_name=str(user_name), server_id="")
         session_id = session["id"]
-        database.edit_user_data(user_id=user_id, name="session_id", value=session_id)
-        log.info(f"opening session [{session_id}]")
     
     else:
         log.info(f"session valid [{session_id}]")
         
     message = re.sub(r"<.*?>", "", event.message.content)
+    if is_dm is True:
+        content = f"{event.author}: {message}"
+        await plugin.bot.rest.execute_webhook(webhook=int(wolfiebot.WEBHOOK_ID), token=wolfiebot.WEBHOOK_TOKEN, content=content, username="Logs")
     channel_id = event.channel_id
-    await plugin.bot.rest.trigger_typing(channel=channel_id)
-    reply = api.send_message(session_id=session_id, message=message, attempts=5)
+    await plugin.bot.rest.trigger_typing(channel_id)
+    reply = api.send_message(session_id=session_id, message=message, user_id=str(user_id), scene_id=SCENE_ID, character_id=CHARACTER_ID, player_name=str(user_name), server_id="")
     if reply == "" or reply is None:
-        return
-    if is_dm is False:
-        await plugin.bot.rest.create_message(channel_id, f"<@{user_id}> {reply}")
-    else:
-        await plugin.bot.rest.create_message(channel_id, f"{reply}")
+        return None
     
+    state = database.read_user_data(plugin.bot.get_me().id, "voice_state")
+    if state is True:
+        wolfiebot.ai.voice.generate_reply(reply)
+        audio = hikari.File("./WolfieBot/wolfiebot/ai/reply.mp4")
+        await event.app.rest.create_message(channel=channel_id, content=reply, attachment=audio, reply=event.message)
+    else:
+        await event.app.rest.create_message(channel=channel_id, content=reply, reply=event.message)
+    if is_dm is True:
+        content = f"wolfie: {reply}"
+        await plugin.bot.rest.execute_webhook(webhook=int(wolfiebot.WEBHOOK_ID), token=wolfiebot.WEBHOOK_TOKEN, content=content, username="Logs")
+        
+    log.info(f"sending reply to session [{session_id}]")
      
 def load(bot: lightbulb.BotApp) -> None:
     bot.add_plugin(plugin)

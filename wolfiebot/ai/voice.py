539d67a23f5744db09927f5222af6c71d79988f6
wolfisonline <cmzpoison@gmail.com>
2023-07-02 18:33:29 -0400

    docstrings added

--- wolfiebot/ai/voice.py
+++ wolfiebot/ai/voice.py
@@ -1,40 +1,58 @@
+"""
+Voice Module
+"""
 import logging
-import hikari
-import wolfiebot
 import lightbulb
 
 from elevenlabs import set_api_key, voices, generate, save
-from moviepy.editor import *
+from moviepy.editor import ImageClip, AudioFileClip
+
+# pylint: disable=import-error
+import wolfiebot
+
 
 log = logging.getLogger(__name__)
 plugin = lightbulb.Plugin("ai.voice")
 set_api_key(wolfiebot.ELEVENLABS_API_KEY)
+BASE_PATH = "./WolfieBot/wolfiebot/content"
+AUDIO_PATH = f"{BASE_PATH}/reply.mp3"
+IMAGE_PATH = f"{BASE_PATH}/wolfie-pfp.png"
+OUTPUT_PATH = f"{BASE_PATH}/reply.mp4"
 
 
-def generate_reply(reply: str):
-    base_path = "./WolfieBot/wolfiebot/content"
-    audio_path = f"{base_path}/reply.mp3"
-    image_path = f"{base_path}/wolfie-pfp.png"
-    output_path = f"{base_path}/reply.mp4"
-    
-    log.info("generating audio")
+def generate_reply(reply: str) -> None:
+    """
+    Generates an audio reply based on the provided text.
+
+    Args:
+        reply (str): The text to generate the audio reply from.
+
+    Returns:
+        None
+    """
+
     voices()
-    audio = generate(text=reply, api_key=wolfiebot.ELEVENLABS_API_KEY, voice="wolfie", model="eleven_multilingual_v1")
-    save(audio, audio_path)
-    
-    audio = AudioFileClip(audio_path)
+    audio = generate(
+        text=reply,
+        api_key=wolfiebot.ELEVENLABS_API_KEY,
+        voice="wolfie",
+        model="eleven_multilingual_v1"
+    )
 
-    log.info("converting audio to mp4")
-    image = ImageClip(image_path)
+    save(audio, AUDIO_PATH)
+    audio = AudioFileClip(AUDIO_PATH)
+
+    image = ImageClip(IMAGE_PATH)
+    # pylint: disable=no-member
     image = image.resize((150, 150))
     image = image.set_duration(audio.duration)
 
     video = image.set_audio(audio)
-    video.write_videofile(output_path, codec='libx264', audio_codec='aac', fps=24, verbose=False, logger=None)
-    
-def load(bot: lightbulb.BotApp):
-    bot.add_plugin(plugin)
-
-def unload(bot: lightbulb.BotApp):
-    bot.remove_plugin(plugin)
-
+    video.write_videofile(
+        OUTPUT_PATH,
+        codec='libx264',
+        audio_codec='aac',
+        fps=24,
+        verbose=False,
+        logger=None
+    )

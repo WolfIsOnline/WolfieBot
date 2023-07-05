576910a77a2f4e08807683f4a026522fba55cb92
wolfisonline <cmzpoison@gmail.com>
2023-07-05 17:25:07 -0400

    documented, refactored

--- wolfiebot/core/welcome.py
+++ wolfiebot/core/welcome.py
@@ -4,8 +4,8 @@ Welcome extension
 import logging
 import hikari
 import lightbulb
-import wolfiebot
 
+# pylint: disable=import-error, no-name-in-module
 from wolfiebot.database.database import Database
 from wolfiebot.ai.api import Api
 
@@ -19,24 +19,29 @@ CHARACTER_ID = "-1"
 
 @plugin.listener(hikari.MemberCreateEvent)
 async def user_join(event):
+    """
+    Handles the user join event and sends a welcome message.
+
+    Args:
+        event: The event object representing the user join event.
+
+    Returns:
+        None
+    """
     user = event.user
     user_id = user.id
     guild_id = event.guild_id
     welcome_channel = database.read_guild_data(guild_id=guild_id, name="welcome_channel")
-    session_id = database.read_user_data(user_id=user_id, name="session_id")
-    session_status = api.get_session_status(session_id)
-    if session_status is True:
-        api.close_session(session_id=session_id)
-
-    session = api.open_session({
-            "user_id": user_id,
-            "scene_id": SCENE_ID,
-            "character_id": CHARACTER_ID,
-            "name": str(user)
-        })
-    session_id = session.get("id")
-    welcome_message = api.send_scene_trigger(session_id=session_id, custom_id="welcome")
-    await plugin.bot.rest.create_message(channel=welcome_channel, content=f"<@{user_id}> {welcome_message}")
+    # pylint: disable=undefined-variable
+    message = await wolfiebot.ai.chat.generate_scene_reply(
+        user_id=user_id,
+        name=user,
+        custom_id="welcome"
+    )
+    await plugin.bot.rest.create_message(
+        channel=welcome_channel,
+        content=f"<@{user_id}> {message}"
+    )
 
 def load(bot: lightbulb.BotApp):
     """

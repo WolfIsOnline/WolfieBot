fb7bdb554af33007136d3ffdf07455c482fb843d
wolfisonline <cmzpoison@gmail.com>
2023-07-05 00:22:17 -0400

    replaced welcome with wolfie welcome

--- wolfiebot/core/welcome.py
+++ wolfiebot/core/welcome.py
@@ -1,31 +1,61 @@
+"""
+Welcome extension
+"""
 import logging
 import hikari
-import wolfiebot
 import lightbulb
+import wolfiebot
 
 from wolfiebot.database.database import Database
+from wolfiebot.ai.api import Api
 
 log = logging.getLogger(__name__)
 plugin = lightbulb.Plugin("core.welcome")
-db = Database()
+database = Database()
+api = Api()
+
+SCENE_ID = "workspaces/default-firdbgosgclm_v_vu5dcnw/scenes/discord"
+CHARACTER_ID = "-1"
 
 @plugin.listener(hikari.MemberCreateEvent)
-async def member_join(event):
-    member = event.user
+async def user_join(event):
+    user = event.user
+    user_id = user.id
     guild_id = event.guild_id
-    guild = plugin.bot.cache.get_guild(guild_id)
-    embed = hikari.Embed(color=wolfiebot.DEFAULT_COLOR, title="Welcome to Nocturnal Gamers", description=f"Hi {member.mention}\n\n• **I hope you enjoy your stay!**\n\n• **Check out the following channels.**\n\n<a:ng_bluearrowright:996149975471902760> Current free games <#967074827108245554>\n\n<a:ng_bluearrowright:996149975471902760> Newest <#973745210016280577>",)
-    embed.set_author(name=f"{guild}", icon=guild.icon_url)
-    embed.set_thumbnail(member.display_avatar_url)
-    try:
-        embed.set_image(guild.banner_url)
-    except:
-        pass
-    
-    await plugin.bot.rest.create_message(db.read_guild_data(guild_id, "welcome_channel"), embed)
-    
+    welcome_channel = database.read_guild_data(guild_id=guild_id, name="welcome_channel")
+    session_id = database.read_user_data(user_id=user_id, name="session_id")
+    session_status = api.get_session_status(session_id)
+    if session_status is True:
+        api.close_session(session_id=session_id)
+
+    session = api.open_session({
+            "user_id": user_id,
+            "scene_id": SCENE_ID,
+            "character_id": CHARACTER_ID,
+            "name": str(user)
+        })
+    session_id = session.get("id")
+    welcome_message = api.send_scene_trigger(session_id=session_id, custom_id="welcome")
+    await plugin.bot.rest.create_message(channel=welcome_channel, content=f"<@{user_id}> {welcome_message}")
+
 def load(bot: lightbulb.BotApp):
+    """
+    Loads the plugin into the bot.
+
+    Parameters:
+    - bot (lightbulb.BotApp): The bot instance.
+
+    """
     bot.add_plugin(plugin)
 
 def unload(bot: lightbulb.BotApp):
+    """
+    Unloads the plugin from the specified bot.
+
+    Args:
+        bot (lightbulb.BotApp): The bot instance to unload the plugin from.
+
+    Returns:
+        None
+    """
     bot.remove_plugin(plugin)

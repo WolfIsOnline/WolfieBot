d4f67c42dc3355c5d25a69c71519378e7fb2094f
wolfisonline <cmzpoison@gmail.com>
2023-07-17 16:23:41 -0400

    added quote commands

---
+++ wolfiebot/commands/quotes.py
@@ -0,0 +1,117 @@
+"""
+Quote Commands
+"""
+import logging
+import random
+from typing import List
+import hikari
+import lightbulb
+
+# pylint: disable=no-name-in-module, import-error, unused-import
+from wolfiebot.database.database import Database
+
+log = logging.getLogger(__name__)
+plugin = lightbulb.Plugin("commands.owner")
+database = Database()
+
+@plugin.command
+@lightbulb.option("user", "Select a member", type=hikari.User, required=False)
+@lightbulb.command("quote", "Get a random quote")
+@lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
+async def quote(ctx: lightbulb.Context):
+    """
+    Get a random quote.
+
+    If a user is specified, it retrieves a random quote from that user.
+    Otherwise, it retrieves a random quote from all quotes.
+
+    Args:
+        ctx (lightbulb.Context): The context object representing the invocation.
+
+    Returns:
+        None
+    """
+    guild_id = ctx.get_guild().id
+    user = ctx.options.user
+    if user is not None:
+        user_quotes = await get_quote_from_user(user_id=user.id, guild_id=guild_id)
+        user_quote = await get_random_quote(quotes=user_quotes)
+    else:
+        wildcard_quote = await get_random_wildcard_quote(guild_id=guild_id)
+        quote_user_id = wildcard_quote.get("quote_user_id")
+        user_quote = wildcard_quote.get("quote")
+        if quote_user_id == -1:
+            name = "Anonymous"
+        else:
+            name = f"<@{quote_user_id}>"
+
+    await ctx.respond(f"\"{user_quote}\" - {name}")
+
+async def get_quote_from_user(user_id: int, guild_id: int) -> List[str]:
+    """
+    Retrieves quotes from a specific user within a guild.
+
+    Args:
+        user_id (int): The ID of the user.
+        guild_id (int): The ID of the guild.
+
+    Returns:
+        List[str]: A list of quotes submitted by the user within the guild.
+    """
+    quotes = database.read_guild_data(guild_id=guild_id, name="quotes")
+    user_quotes = []
+    for _quote in quotes:
+        if _quote.get("quote_user_id") == user_id:
+            user_quotes.append(_quote.get("quote"))
+    return user_quotes
+
+async def get_random_wildcard_quote(guild_id: int):
+    """
+    Retrieves a random quote from the wildcard quotes in a guild.
+
+    Args:
+        guild_id (int): The ID of the guild.
+
+    Returns:
+        dict: A random wildcard quote from the guild.
+    """
+    quotes = database.read_guild_data(guild_id=guild_id, name="quotes")
+    _quote = random.choice(quotes)
+    return _quote
+
+async def get_random_quote(quotes: List[str]) -> str:
+    """
+    Retrieves a random quote from the given list of quotes.
+
+    Args:
+        quotes (List[str]): A list of quotes.
+
+    Returns:
+        str: A random quote from the list.
+    """
+    _quote = random.choice(quotes)
+    return _quote
+
+def load(bot: lightbulb.BotApp) -> None:
+    """
+    Loads the plugin into the bot.
+
+    Args:
+        bot (lightbulb.BotApp): The bot instance.
+
+    Returns:
+        None
+    """
+    bot.add_plugin(plugin)
+
+def unload(bot: lightbulb.BotApp) -> None:
+    """
+    Unloads the plugin from the bot.
+
+    Args:
+        bot (lightbulb.BotApp): The bot instance.
+
+    Returns:
+        None
+    """
+    bot.remove_plugin(plugin)

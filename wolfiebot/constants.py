8fb94411d1315d9ad0af7dcd5771b7f1010b740d
wolfisonline <cmzpoison@gmail.com>
2024-01-30 18:30:08 -0500

    constants vars

---
+++ wolfiebot/constants.py
@@ -0,0 +1,27 @@
+"""Mostly constant strings"""
+
+INSUFFICIENT_FUNDS_ERROR = "Declined, you do not have enough money"
+NEGATIVE_AMOUNT_ERROR = "Declined, amount can't be negative"
+INSUFFICIENT_CREDITS_ERROR = "Declined, you do not have enough credits"
+BANK_DEPOSIT = "has been deposited into your account"
+BANK_TRANSFER = "has been transferred"
+SLOTS_CMD_AMOUNT_DESC = "How many credits you want to bet."
+SLOTS_CMD_DESC = "Spin the reels and see how much you win."
+
+CASINO_MIN_GAMBLE = "Minimum bet is ₵2"
+SLOTS_SYM_DIAMOND_ID = 1117258226426191882
+SLOTS_SYM_DIAMOND_WEIGHT = 6
+SLOTS_SYM_CHERRY_ID = 1117258227256655922
+SLOTS_SYM_CHERRY_WEIGHT = 19
+SLOTS_SYM_SEVEN_ID = 1117258223662149734
+SLOTS_SYM_SEVEN_WEIGHT = 13
+SLOTS_SYM_BLANK_ID = 1118195008013611048
+SLOTS_SYM_BLANK_WEIGHT = 50
+SLOTS_SYM_BAR_ID = 1117258225528606730
+SLOTS_SYM_BAR_WEIGHT = 14
+SLOTS_SYM_ANIMATION_ID = 1117230452802060328
+
+
+USER_SELECT_DESC = "Select the user"
+MAYA_CLEAR_MEMORY_DESC = "Clears my short-term memory."
+MAYA_CLEAR_MEMORY = "My short-term memory has been cleared"

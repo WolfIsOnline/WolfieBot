14661c0cc998e273c8fafd2c75ecf6e2f7e57258
wolfisonline <cmzpoison@gmail.com>
2024-02-03 07:19:49 -0500

    track time spent in VC

--- wolfiebot/core/stats.py
+++ wolfiebot/core/stats.py
@@ -1,10 +1,16 @@
 """Stat tracking"""
+
+import datetime
+
 import hikari
 import lightbulb
 
+import wolfiebot
 from wolfiebot.database.database import UserData, GuildData
+from wolfiebot.logger import Logger
 
 plugin = lightbulb.Plugin("core.stats")
+log = Logger(__name__, wolfiebot.LOG_LEVEL)
 
 
 class UserStats:
@@ -35,6 +41,52 @@ class GuildStats:
         self.guild_data.edit(name="messages", value=new_value)
 
 
+class Timer:
+    _instances = {}
+
+    def __new__(cls, timer_id: int):
+        if timer_id not in cls._instances:
+            cls._instances[timer_id] = super(Timer, cls).__new__(cls)
+        return cls._instances[timer_id]
+
+    def __init__(self, timer_id: int):
+        if not hasattr(self, "initialized"):
+            self.time = 0
+            self._start_time = None
+            self.id = timer_id
+            self.is_running = False
+            self.initialized = True
+            log.debug("new timer created")
+
+    def start(self):
+        if not self.is_running:
+            self._start_time = datetime.datetime.now()
+            self.is_running = True
+
+    def stop(self):
+        if self.is_running:
+            elapsed_time = datetime.datetime.now() - self._start_time
+            self.time = round(elapsed_time.total_seconds())
+            self.is_running = False
+
+
+@plugin.listener(hikari.VoiceStateUpdateEvent)
+async def on_voice(event):
+    user_id = event.state.user_id
+    if user_id == plugin.bot.get_me().id:
+        return None
+
+    user_timer = Timer(timer_id=user_id)
+
+    if event.state.channel_id is not None:
+        if not user_timer.is_running:
+            user_timer.start()
+    else:
+        if user_timer.is_running:
+            user_timer.stop()
+            log.debug("user spent: %s", user_timer.time)
+
+
 @plugin.listener(hikari.GuildMessageCreateEvent)
 async def on_message(event):
     if event.is_bot is True or event.message.content.startswith("!"):
@@ -51,8 +103,7 @@ async def on_message(event):
 
 
 def load(bot: lightbulb.BotApp) -> None:
-    """
-    Loads the plugin into the bot.
+    """Loads the plugin into the bot.
 
     Args:
         bot (lightbulb.BotApp): The bot instance.
@@ -64,8 +115,7 @@ def load(bot: lightbulb.BotApp) -> None:
 
 
 def unload(bot: lightbulb.BotApp) -> None:
-    """
-    Unloads the plugin from the bot.
+    """Unloads the plugin from the bot.
 
     Args:
         bot (lightbulb.BotApp): The bot instance.
